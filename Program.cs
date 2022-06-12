1709dfcdba0285dc8d04e108be4482bd1dfabb2a
Kirill Tronin <troninkirill@gmail.com>
2022-06-12 14:23:29 +0100

    cleanup

--- Program.cs
+++ Program.cs
@@ -3,28 +3,25 @@
 const int start = 10;
 const int count = 10;
 
-List<int> getSequence(int num)
+IEnumerable<int> getSequence(int num)
 {
-    var steps = new List<int>() { num };
-
-    while (steps[steps.Count - 1] != 1)
+    while (num != 1)
     {
-        var lastNumber = steps[steps.Count - 1];
-        steps.Add(lastNumber % 2 == 0 ? lastNumber / 2 : lastNumber * 3 + 1);
+        num = num % 2 == 0 ? num / 2 : num * 3 + 1;
+        yield return num;
     }
-
-    return steps;
 }
 
 Chart.Combine(
     Enumerable.Range(start, count)
     .ToList()
-    .Select(num => {
+    .Select(num =>
+    {
         var seq = getSequence(num);
         var chart = Chart2D.Chart.Line<int, int, int>(
-            x: Enumerable.Range(0, seq.Count), 
-            y: seq, 
-            ShowLegend: true, 
+            x: Enumerable.Range(0, seq.ToList().Count),
+            y: seq,
+            ShowLegend: true,
             Name: $"Number {num}");
         return chart;
     })
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Collatz Conjecture
 
-inspired by https://www.youtube.com/watch?v=094y1Z2wpJg
+Inspired by https://www.youtube.com/watch?v=094y1Z2wpJg
 
 # Run
 
